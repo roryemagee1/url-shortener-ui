344e4ef9646901808ef82431376de1c6f5ede3d3
Rory Magee <roryemagee@gmail.com>
2022-04-26 11:43:34 -0600

    Complete first test

--- cypress/integration/HomeSpec.js
+++ cypress/integration/HomeSpec.js
@@ -1,7 +1,44 @@
 describe("Home Page Tests", () => {
 
-  it('Should run this test', () => {
+  it('When a user visits the page, they can view the page title and the existing shortened URLs', () => {
+    cy.visit('http://localhost:3000/')
+    cy.intercept('GET', `http://localhost:3001/api/v1/urls`, {
+      fixture: 'getData.json'
+      }).as('getData');
+
+    cy.get('h1')
+      .contains('URL Shortener')
+
+    cy.get('.container')
+      .children()
+      .should('have.length', 3);
+
+    cy.get('.container')
+      .get('.url').eq(0)
+      .get('a')
+        .should('have.attr', 'href')
+        .and('eq', 'http://localhost:3001/useshorturl/1');
+      
+    cy.get('.container')
+      .get('.url').eq(1)
+      .get('a')
+        .should('have.attr', 'href')
+        .and('eq', 'http://localhost:3001/useshorturl/1');
     
+    cy.get('.container')
+      .get('.url').eq(2)
+      .get('a')
+        .should('have.attr', 'href')
+        .and('eq', 'http://localhost:3001/useshorturl/1');
+
+  })
+
+  it('When a user visits the page, they can view the Form with the proper inputs', () => {
+
+  })
+
+  it('When a user fills out the form, the information is reflected in the input fields', () => {
+
   })
 
 })
--- src/components/UrlContainer/UrlContainer.js
+++ src/components/UrlContainer/UrlContainer.js
@@ -13,7 +13,7 @@ const UrlContainer = ({ urls }) => {
   });
 
   return (
-    <section>
+    <section className="container">
       { urlEls.length ? urlEls : <p>No urls yet! Find some to shorten!</p> }
     </section>
   )
