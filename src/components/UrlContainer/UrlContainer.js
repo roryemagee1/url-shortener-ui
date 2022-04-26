66d35d57b92c000a49c96278eda9f44ca63b0794
Rory Magee <roryemagee@gmail.com>
2022-04-26 10:00:25 -0600

    Add temporary key-id pair in UrlContainer

--- src/components/UrlContainer/UrlContainer.js
+++ src/components/UrlContainer/UrlContainer.js
@@ -2,10 +2,9 @@ import React from 'react';
 import './UrlContainer.css';
 
 const UrlContainer = ({ urls }) => {
-  console.log(urls);
-  let urlEls = urls.map(url => {
+  let urlEls = urls.map((url, i) => {
     return (
-      <div className="url">
+      <div className="url" key={i} id={i}>
         <h3>{url.title}</h3>
         <a href={url.short_url} target="blank">{url.short_url}</a>
         <p>{url.long_url}</p>
